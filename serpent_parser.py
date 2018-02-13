2f5e10d952b09317b5cc30f60ecb281015656e09
Jin Whan Bae <jbae11@illinois.edu>
2018-02-13 11:24:35 -0600

    keff and sd from res file

--- serpent_parser.py
+++ serpent_parser.py
@@ -63,13 +63,13 @@ def filter_trace(comp_dict, percent_cutoff):
 
 
 
-def bumat_read(filename):
+def bumat_read(bumat_file):
     """reads serpent .bumat output file and 
        stores the composition in a dictionary
 
     Parameters
     ----------
-    filename: str
+    bumat_file: str
         bumat file path
 
     Returns
@@ -78,13 +78,13 @@ def bumat_read(filename):
         dictionary of composition
         (key=isotope(ZZAAA), value=atomic density)
     """
-    with open(filename) as f:
-        useful = f.readlines()[5:]
+    with open(bumat_file) as f:
+        comp_lines = f.readlines()[5:]
 
     comp_dict = {}
-    header = useful[0]
-    for i in range(1, len(useful)):
-        parsed = parse_line(useful[i])
+    header = comp_lines[0]
+    for i in range(1, len(comp_lines)):
+        parsed = parse_line(comp_lines[i])
         # isotope as key, atomic density as value
         comp_dict[parsed[0]] = parsed[1]
 
@@ -92,7 +92,61 @@ def bumat_read(filename):
     return comp_dict
 
 
-def csv_render(csv_filename, dictionary):
+def search_keff(res_file):
+    """searches and returns the mean keff value in the .res file
+
+    Parameters
+    ----------
+    res_file: str
+        path to .res file
+
+    Returns
+    -------
+    keff_dict: dict
+        keff and std (key = keff or sd, value = list of keff or sd)
+    """
+    with open(res_file) as f:
+        lines = f.readlines()
+
+    keff_list = []
+    sd_list = []
+
+    for i in range(0, len(lines)):
+        if 'IMP_KEFF' in lines[i]:
+            keff_list.append(keff_line_parse(lines[i])[0])
+            sd_list.append(keff_line_parse(lines[i])[1])
+
+    keff_dict = {}
+    keff_dict['keff'] = keff_list 
+    keff_dict['sd'] = sd_list
+    return keff_dict
+
+
+def keff_line_parse(keff_line):
+    """parses through the ana_keff line in .res file
+
+    Parameters
+    ----------
+    keff_line: str
+        string from .res file listing IMP_KEFF
+
+    Returns
+    -------
+    tuple
+        (mean IMP_KEFF, std deviation of IMP_KEFF)
+    """
+    start = keff_line.find('=')
+    new_keff_line = keff_line[start:]
+    start = new_keff_line.find('[')
+    end = new_keff_line.find(']')
+    
+    # +3 and -1 is to get rid of leading and trailing whitespace
+    keff_sd = new_keff_line[start + 3:end - 1]
+    (keff, sd) = keff_sd.split(' ')
+    return (keff, sd)
+
+
+def csv_render_dict(csv_filename, dictionary, header):
     """renders csv given the dictionary
        column 1 = key, column 2 = value
     
@@ -102,6 +156,8 @@ def csv_render(csv_filename, dictionary):
         path of csv file to be created
     dictionary: dict
         dictionary to be rendered into csv file
+    header: list
+        list of length 2 of header strings
 
     Returns
     -------
@@ -110,11 +166,54 @@ def csv_render(csv_filename, dictionary):
     with open(csv_filename, 'w') as csv_file:
         writer = csv.writer(csv_file)
         # write header
-        writer.writerow(['Isotope', 'Atomic Density'])
+        writer.writerow(header)
         for key, value in dictionary.items():
             writer.writerow([key, value])
-
     return True
 
+
+def csv_render_list_dict(csv_filename, list_dict):
+    """renders csv given list of data
+       column 1 = entry number, column 2+ = values
+
+    Parameters
+    ----------
+    csv_filename: str
+        path of csv file to be created
+    list_dict: dictionary
+        dictionary with lists of values to be rendered
+
+    Returns
+    -------
+    true if successful
+    """
+    with open(csv_filename, 'w') as csv_file:
+        writer = csv.writer(csv_file)
+        # write header
+        header_list = ['number']
+        header_list.extend(list(list_dict.keys()))
+        writer.writerow(header_list)
+
+        # check if lengths of all lists are the same
+        length_list = []
+        key_list = []
+        for key, value in list_dict.items():
+           length_list.append(len(value))
+           key_list.append(key)
+        if len(set(length_list)) != 1:
+            raise ValueError('Lists have to be the same length') 
+
+        count = 0
+        for i in range(0, length_list[0]):
+            temp_list = [count + 1]
+            for key_index in range(0, len(key_list)):
+                temp_list.append(list_dict[key_list[key_index]][i])
+            writer.writerow(temp_list)
+            count =+ 1
+
+        return True
+
 comp_dict = bumat_read('./output/publ_core.bumat1')
-csv_render('composition.csv', comp_dict)
+csv_render_dict('composition.csv', comp_dict, ['Isotope', 'Atomic Density'])
+keff_dict = search_keff('./output/publ_core_res.m')
+csv_render_list_dict('keff.csv', keff_dict)
