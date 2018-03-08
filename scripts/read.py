fe0b154715f5e1bd9c65cff377d948e93aba8539
Jin Whan Bae <jbae11@illinois.edu>
2018-03-08 15:39:04 -0600

    read csv files

--- scripts/read.py
+++ scripts/read.py
@@ -1,9 +1,23 @@
 import h5py
 import numpy as np
+import csv
 
-filename = 'db_modified.hdf5'
-f = h5py.File(filename, 'r')
+def read_hdf5(filename):
+    f = h5py.File(filename, 'r')
 
-for key in f:
-    print(key)
-    print(f[key])
+    for key in f:
+        print(key)
+        print(f[key])
+
+def read_csv():
+    filename = '../rom/results_dump.csv'
+    with open(filename, mode='r') as input_file:
+        for i, line in enumerate(input_file):
+            if i == 0:
+                header = line.split(',')
+            if i == 1:
+                value = line.split(',')
+    for i in range(len(header)):
+        print('%s : %s \n' %(header[i], value[i]))
+
+read_csv()
