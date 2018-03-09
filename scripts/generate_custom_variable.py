798eb840ee97332d29b00d5083e2e88e82a3d004
Jin Whan Bae <jbae11@illinois.edu>
2018-03-09 13:53:40 -0600

    full name for isotope:

--- scripts/generate_custom_variable.py
+++ scripts/generate_custom_variable.py
@@ -7,5 +7,14 @@ with open(file, 'r') as isofile:
 custom_variable_file = '../custom_xml'
 with open(custom_variable_file, 'w') as cvf:
     for iso in isotopes:
-        print(iso)
         cvf.write('<variable name="f' + iso + '"/> \n')
+
+feature_space = '../feature_space'
+with open(feature_space, 'w') as fs:
+    for iso in isotopes:
+        fs.write('f'+iso+',')
+
+target_space = '../target_space'
+with open(target_space, 'w') as ts:
+    for iso in isotopes:
+        ts.write('d'+iso+',')
