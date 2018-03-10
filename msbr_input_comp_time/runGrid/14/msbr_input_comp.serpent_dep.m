e2863546805562629fcb3aa2e041e7e5f6e8395c
Jin Whan Bae <jbae11@illinois.edu>
2018-03-09 21:02:33 -0600

    time dependence startup

---
+++ msbr_input_comp_time/runGrid/14/msbr_input_comp.serpent_dep.m
@@ -0,0 +1,105 @@
+
+ZAI = [
+666
+0
+];
+
+NAMES = [
+'lost            '
+'total           '
+];
+
+iLOST    = 1;
+iTOT     = 2;
+
+MAT_fuel_VOLUME = [ 1.95057E+07 1.95057E+07 ];
+
+MAT_fuel_BURNUP = [ 0.00000E+00 2.00079E-02 ];
+
+MAT_fuel_ADENS = [
+0.00000E+00 0.00000E+00 % lost data
+1.72534E-01 1.72534E-01 % total
+];
+
+MAT_fuel_MDENS = [
+0.00000E+00 0.00000E+00 % lost data
+3.33040E+00 3.33040E+00 % total
+];
+
+MAT_fuel_A = [
+0.00000E+00 0.00000E+00 % lost data
+1.23797E+14 1.76314E+19 % total
+];
+
+MAT_fuel_H = [
+0.00000E+00 0.00000E+00 % lost data
+9.74474E+01 6.43175E+06 % total
+];
+
+MAT_fuel_SF = [
+0.00000E+00 0.00000E+00 % lost data
+1.44964E+00 6.85295E+01 % total
+];
+
+MAT_fuel_GSRC = [
+0.00000E+00 0.00000E+00 % lost data
+1.04637E+13 1.66479E+19 % total
+];
+
+MAT_fuel_ING_TOX = [
+0.00000E+00 0.00000E+00 % lost data
+6.36141E+06 2.65099E+11 % total
+];
+
+MAT_fuel_INH_TOX = [
+0.00000E+00 0.00000E+00 % lost data
+1.21502E+09 2.69821E+11 % total
+];
+
+TOT_VOLUME = [ 1.95057E+07 1.95057E+07 ];
+
+TOT_BURNUP = [ 0.00000E+00 2.00079E-02 ];
+
+TOT_ADENS = [
+0.00000E+00 0.00000E+00 % lost data
+1.72534E-01 1.72534E-01 % total
+];
+
+TOT_MASS = [
+0.00000E+00 0.00000E+00 % lost data
+6.49618E+07 6.49618E+07 % total
+];
+
+TOT_A = [
+0.00000E+00 0.00000E+00 % lost data
+1.23797E+14 1.76314E+19 % total
+];
+
+TOT_H = [
+0.00000E+00 0.00000E+00 % lost data
+9.74474E+01 6.43175E+06 % total
+];
+
+TOT_SF = [
+0.00000E+00 0.00000E+00 % lost data
+1.44964E+00 6.85295E+01 % total
+];
+
+TOT_GSRC = [
+0.00000E+00 0.00000E+00 % lost data
+1.04637E+13 1.66479E+19 % total
+];
+
+TOT_ING_TOX = [
+0.00000E+00 0.00000E+00 % lost data
+6.36141E+06 2.65099E+11 % total
+];
+
+TOT_INH_TOX = [
+0.00000E+00 0.00000E+00 % lost data
+1.21502E+09 2.69821E+11 % total
+];
+
+BU = [ 0.00000E+00 1.99980E-02 ];
+
+DAYS = [ 0.00000E+00 3.00000E+00 ];
---
+++ msbr_input_comp_time/runGrid/5/msbr_input_comp.serpent_dep.m
@@ -0,0 +1,105 @@
+
+ZAI = [
+666
+0
+];
+
+NAMES = [
+'lost            '
+'total           '
+];
+
+iLOST    = 1;
+iTOT     = 2;
+
+MAT_fuel_VOLUME = [ 1.95057E+07 1.95057E+07 ];
+
+MAT_fuel_BURNUP = [ 0.00000E+00 2.00078E-02 ];
+
+MAT_fuel_ADENS = [
+0.00000E+00 0.00000E+00 % lost data
+1.72411E-01 1.72411E-01 % total
+];
+
+MAT_fuel_MDENS = [
+0.00000E+00 0.00000E+00 % lost data
+3.33040E+00 3.33040E+00 % total
+];
+
+MAT_fuel_A = [
+0.00000E+00 0.00000E+00 % lost data
+1.34086E+14 1.73971E+19 % total
+];
+
+MAT_fuel_H = [
+0.00000E+00 0.00000E+00 % lost data
+1.05546E+02 6.33217E+06 % total
+];
+
+MAT_fuel_SF = [
+0.00000E+00 0.00000E+00 % lost data
+1.44964E+00 6.34155E+01 % total
+];
+
+MAT_fuel_GSRC = [
+0.00000E+00 0.00000E+00 % lost data
+1.13336E+13 1.66243E+19 % total
+];
+
+MAT_fuel_ING_TOX = [
+0.00000E+00 0.00000E+00 % lost data
+6.88614E+06 2.66350E+11 % total
+];
+
+MAT_fuel_INH_TOX = [
+0.00000E+00 0.00000E+00 % lost data
+1.31379E+09 2.70755E+11 % total
+];
+
+TOT_VOLUME = [ 1.95057E+07 1.95057E+07 ];
+
+TOT_BURNUP = [ 0.00000E+00 2.00078E-02 ];
+
+TOT_ADENS = [
+0.00000E+00 0.00000E+00 % lost data
+1.72411E-01 1.72411E-01 % total
+];
+
+TOT_MASS = [
+0.00000E+00 0.00000E+00 % lost data
+6.49618E+07 6.49618E+07 % total
+];
+
+TOT_A = [
+0.00000E+00 0.00000E+00 % lost data
+1.34086E+14 1.73971E+19 % total
+];
+
+TOT_H = [
+0.00000E+00 0.00000E+00 % lost data
+1.05546E+02 6.33217E+06 % total
+];
+
+TOT_SF = [
+0.00000E+00 0.00000E+00 % lost data
+1.44964E+00 6.34155E+01 % total
+];
+
+TOT_GSRC = [
+0.00000E+00 0.00000E+00 % lost data
+1.13336E+13 1.66243E+19 % total
+];
+
+TOT_ING_TOX = [
+0.00000E+00 0.00000E+00 % lost data
+6.88614E+06 2.66350E+11 % total
+];
+
+TOT_INH_TOX = [
+0.00000E+00 0.00000E+00 % lost data
+1.31379E+09 2.70755E+11 % total
+];
+
+BU = [ 0.00000E+00 1.99980E-02 ];
+
+DAYS = [ 0.00000E+00 3.00000E+00 ];
