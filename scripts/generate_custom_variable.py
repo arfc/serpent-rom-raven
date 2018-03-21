678814d4b6ed520a1bac28f9b8c36681349400c9
Jin Whan Bae <jbae11@illinois.edu>
2018-03-21 12:40:27 -0500

    gen script edit

--- scripts/generate_custom_variable.py
+++ scripts/generate_custom_variable.py
@@ -1,20 +1,22 @@
-file = '../iso_file'
+
+file = '../aux-input-files/iso_file'
 isotopes = []
+
 with open(file, 'r') as isofile:
     for line in isofile:
         isotopes.append(line.strip())
 
-custom_variable_file = '../custom_xml'
+custom_variable_file = '../aux-input-files/custom_variable.xml'
 with open(custom_variable_file, 'w') as cvf:
     for iso in isotopes:
         cvf.write('<variable name="f' + iso + '"/> \n')
 
-feature_space = '../feature_space'
+feature_space = '../aux-input-files/feature_isotopes'
 with open(feature_space, 'w') as fs:
     for iso in isotopes:
         fs.write('f'+iso+',')
 
-target_space = '../target_space'
+target_space = '../aux-input-files/target_isotopes'
 with open(target_space, 'w') as ts:
     for iso in isotopes:
         ts.write('d'+iso+',')
