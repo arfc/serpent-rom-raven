d32ac9967484d0846e8145c7b11e4dc484f0d0f7
Jin Whan Bae <jbae11@illinois.edu>
2018-03-25 14:49:00 -0500

    external driver comments

--- external_rom_use/ext_rom.py
+++ external_rom_use/ext_rom.py
@@ -3,11 +3,11 @@ sys.path.append("/projects/sciteam/bahg/projects/raven/scripts")
 from externalROMloader import ravenROMexternal
 import numpy as np 
 
-
-rom = ravenROMexternal("/projects/sciteam/bahg/projects/raven/framework/CodeInterfaces/SERPENT/test/rom/keff_rom_svm.pk",
+# ravenROMexternal(path/to/rom, path/to/raven/framework)
+rom = ravenROMexternal("/projects/sciteam/bahg/projects/raven/framework/CodeInterfaces/SERPENT/external_rom_use/rom/keff_rom_svm.pk",
                        "/projects/sciteam/bahg/projects/raven/framework")
 
-
+# read csv file to create input dictionary
 with open('../rom/test.csv', 'r') as out:
     lines = out.readlines()
     keys = lines[0].split(',')
@@ -20,4 +20,7 @@ with open('../rom/test.csv', 'r') as out:
     print(dic)
 
 eval = rom.evaluate(dic)
+# eval returns a list of dictionarys
+# where one element(dictionary) represents the ROM result
+# of the corresponding input element 
 print(eval)
