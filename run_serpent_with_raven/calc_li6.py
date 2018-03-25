ed854756de50998592394c19d96ad883439df7d0
Jin Whan Bae <jbae11@illinois.edu>
2018-03-25 14:09:01 -0500

    function to calculate adens

--- run_serpent_with_raven/calc_li6.py
+++ run_serpent_with_raven/calc_li6.py
@@ -1,5 +1,3 @@
 import mass_frac_calc
 def evaluate(self):
-    key = __file__.split('_')[1]
-    key = key.split('.')[0]
-    return mass_frac_calc.return_value(key, self.u233_mole_frac)
+    return mass_frac_calc.return_value('li6', self.u233_mole_frac)
--- run_serpent_with_raven/mass_frac_calc.py
+++ run_serpent_with_raven/mass_frac_calc.py
@@ -14,7 +14,6 @@ def return_value(key, u233_mole_frac):
     li7_iso_frac = 0.999952
     li6_iso_frac = 1 - li7_iso_frac
 
-    print(mole_frac_dict.values())
     mass_dict = {}
     mass_dict['li7'] = (mole_frac_dict['lif']) * li7_iso_frac * 7.016 
     mass_dict['li6'] = (mole_frac_dict['lif'])  * li6_iso_frac * 7.016
@@ -27,7 +26,6 @@ def return_value(key, u233_mole_frac):
     mass_dict['be9'] =  (mole_frac_dict['bef2']) * 9.0121
     mass_dict['th232'] = (mole_frac_dict['thf4']) * 232.038
     mass_dict['u233'] = (mole_frac_dict['uf4']) * 233.0396
-    print(mass_dict.values())
     total_mass = sum(mass_dict.itervalues())
 
     mass_frac_dict = {k: v / total_mass * 100.0 * -1 for k, v in mass_dict.iteritems() }
